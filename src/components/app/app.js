ecaf6329992177b1f3b16cc557527435e00a142a
Mirodil Fayzullayev <odilfayz22@gmail.com>
2019-10-02 16:18:01 +0500

    .

---
+++ src/components/app/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+
+    return(
+        <h1>ИНТЕРНЕТ БАНКИНГ для КОРПОРАТИВНЫХ КЛИЕНТОВ</h1>
+    )
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import App from './components/app';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__: null || compose;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootReducer = combineReducers({
+
+});
+
+// const sagaMiddleware = createSagaMiddleware();
+
+const  store = createStore(
+    rootReducer,
+    // composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+// sagaMiddleware.run("sagaName");
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+    ,
+    document.getElementById('root')
+);
+
+serviceWorker.register();
